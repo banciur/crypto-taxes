620b165763156e2d61178fc2e4d50bfd28cb14d8
Banciur <tomek@banciur.org>
2026-01-24 15:23:29 +0100

    Base endpoints

--- data/src/api/api.py
+++ data/src/api/api.py
@@ -3,11 +3,12 @@ from typing import Annotated, AsyncGenerator
 
 from fastapi import Depends, FastAPI
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
-from api.dependencies import get_session
+from api.dependencies import get_corrected_events_repository, get_raw_events_repository, get_seed_events_repository
 from config import DB_FILE
-from db.repositories import CorrectedLedgerEventRepository
+from db.repositories import CorrectedLedgerEventRepository, LedgerEventRepository, SeedEventRepository
+from domain.correction import SeedEvent
 from domain.ledger import LedgerEvent
 
 
@@ -22,7 +23,18 @@ async def lifespan(fastapi_app: FastAPI) -> AsyncGenerator[None, None]:
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/events")
-def get_events(session: Annotated[Session, Depends(get_session)]) -> list[LedgerEvent]:
-    r = CorrectedLedgerEventRepository(session)
-    return r.list()
+@app.get("/raw-events")
+def get_raw_events(rr: Annotated[LedgerEventRepository, Depends(get_raw_events_repository)]) -> list[LedgerEvent]:
+    return rr.list()
+
+
+@app.get("/corrected-events")
+def get_corrected_events(
+    cr: Annotated[CorrectedLedgerEventRepository, Depends(get_corrected_events_repository)],
+) -> list[LedgerEvent]:
+    return cr.list()
+
+
+@app.get("/seed-events")
+def get_seed_events(sr: Annotated[SeedEventRepository, Depends(get_seed_events_repository)]) -> list[SeedEvent]:
+    return sr.list()
